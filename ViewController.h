d8be5dc2bbf09ab91ddcb23547707e94b3e71195
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2017-12-31 14:00:03 -0600

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  Demo App
+//
+//  Created by Jordan Hoehne on 12/31/17.
+//  Copyright © 2017 Jordan Hoehne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+//Naming the features of the app
+@property (weak, nonatomic) IBOutlet UILabel *deviceType;
+@property (weak, nonatomic) IBOutlet UILabel *iOSVersion;
+
+@end
+
